b0e22961d36a390172ac03aa9c6a1c30606fe4fe
Abdulbaki Öztürk <aozturk@nuevo.com.tr>
2023-07-04 22:34:55 +0300

    Add project files.

---
+++ Infrastructure/ETicaretAPI.Persistence/Concretes/ProductService.cs
@@ -0,0 +1,15 @@
+﻿using ETicaretAPI.Application.Abstractions;
+using ETicaretAPI.Domain.Entities;
+
+namespace ETicaretAPI.Persistence.Concretes
+{
+    public class ProductService : IProductService
+    {
+        public List<Product> GetProducts() => new()
+        {
+            new(){ Id=Guid.NewGuid(), Name="Product1",Price=100,Stock=10,CreatedDate=DateTime.Now},
+            new(){ Id=Guid.NewGuid(), Name="Product2",Price=200,Stock=20,CreatedDate=DateTime.Now},
+            new(){ Id=Guid.NewGuid(), Name="Product3",Price=300,Stock=30,CreatedDate=DateTime.Now},
+        };
+    }
+}
